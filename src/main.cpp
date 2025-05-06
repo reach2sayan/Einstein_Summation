c2486ff2b951effe4e4110ed67e9873789ea1d45
Sayan <reach2sayan@gmail.com>
2025-05-06 19:19:15 -0400

    reduce main

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,9 @@
 #include <iostream>
 #include <vector>
 
-using MatA = EinsumTraits::Matrix<int, 2, 2>;
-using MatB = EinsumTraits::Matrix<int, 2, 2>;
-using LabelsA = EinsumTraits::Labels<'i', 'j'>;
-using LabelsB = EinsumTraits::Labels<'j', 'k'>;
-using LabelsR = EinsumTraits::Labels<'i', 'k'>;
-const char strp[4] = "ijk";
-std::string_view str(strp);
-
 template <typename...> struct TD;
 
-using A = EinsumTraits::Labels<'i', 'j'>;
-using B = EinsumTraits::Labels<'j', 'k'>;
-using Res = EinsumTraits::Labels<'i', 'k'>;
-
-using collapsed = EinsumTraits::collapsed_dimensions<A, B, Res>::type;
-
 void second_test() {
-
   std::vector A2{1, 4, 1, 7, 8, 1, 2, 2, 7, 4, 3, 4, 2, 4, 7, 3};
   std::vector B2{2, 5, 0, 1, 5, 7, 9, 2, 2, 3, 5, 1, 7, 5, 6, 3};
   std::mdspan<int, std::extents<size_t, 2, 2, 2, 2>> mdA2{A2.data()};
@@ -51,18 +36,7 @@ void third_test() {
   }
 }
 
-void fourth_test() {
-
-  for (auto i = 0; i < 2; ++i) {
-    for (auto j = 0; j < 2; ++j) {
-      std::cout << res[i,j] << ", ";
-    }
-    std::cout << "\n";
-  }
-}
-
 int main() {
   second_test();
   third_test();
-  fourth_test();
 }
